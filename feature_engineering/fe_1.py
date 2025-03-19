0b3f211f66f4ed25bb9f74d57bb466bd4f811598
SALEEM KHAN <SALEEEEEEEEEM@EXAMPLE.COM>
2025-03-19 22:41:29 +0000

    postgres

--- feature_engineering/fe_1.py
+++ feature_engineering/fe_1.py
@@ -1,5 +1,8 @@
 import pandas as pd
-# connect to postgres
+# connect to postgres database
+    # use script parameters for connection string
 # select data
+    # table is SELECT time_start, temperature_2m, rainfall FROM public.historic_weather;
 # FE columns
+# random transformations such as split by hour etc
 # write to temporary location
